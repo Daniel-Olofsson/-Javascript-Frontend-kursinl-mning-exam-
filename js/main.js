b11e92ff9f016c07eb0efa5abe85a0faa2af174d
Daniel-Olofsson <daniel-olofsson@hotmail.se>
2022-11-09 11:58:10 +0100

    added shopping cart, need delete func

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
+let recipeName = document.getElementById("recipe-name");
 let url = "https://themealdb.com/api/json/v1/1/search.php?s=";
+
+let varukorg = [];
+
 let getInfo = () => {
   let userInp = document.getElementById("user-inp").value;
   if (userInp.length == 0) {
@@ -13,6 +17,9 @@ let getInfo = () => {
         console.log(data);
         console.log(data.meals[0]);
         let myMeal = data.meals[0];
+
+        varukorg.push(myMeal);
+
         console.log(myMeal.strMeal);
         console.log(myMeal.strMealThumb);
         console.log(myMeal.strInstructions);
@@ -32,18 +39,22 @@ let getInfo = () => {
             ingredients.push(`${measure} ${ingredient}`);
           }
         }
-        // modal
+
+        console.log(varukorg);
+   
         //document.cookie = `cart=${ingredients} "SameSite=None"; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/`;
         //allCookies += document.cookie;
-        let modal_titel = document.createElement('h1');
-        document.getElementById('modalcontent').appendChild(modal_titel);
-        modal_titel.innerHTML = myMeal.strMeal;
+        
         //local storage for recipe list
-        const storageData = localStorage.getItem('Varukorg');
-        document.getElementById('modalcontent').innerHTML += storageData;
-        localStorage.setItem('Varukorg',ingredients);
-        console.log(storageData);
+        //localStorage.setItem('Varukorg',ingredients);
+        //localStorage.setItem('foodName', myMeal.strMeal);
+        //recipeList.innerHTML += myMeal.strMeal + `<br></br>`;
+        //recipeName.innerHTML +=  myMeal.strMeal + `<br></br>`+ ingredients + `<br></br>`;
+        displayVarukorg();
+        //log
+      
         console.log(ingredients);
+        //results for user search
         result.innerHTML = `
       <img src=${myMeal.strMealThumb}>
       <h2>${myMeal.strMeal}</h2>
@@ -58,8 +69,6 @@ let getInfo = () => {
           let listItem = document.createElement("li");
           listItem.innerText = item;
           ingredientsCon.appendChild(listItem);
-          //display modal button
-          //document.getElementById("myBtn").style.display = "block";
         });
         
       })
@@ -68,27 +77,42 @@ let getInfo = () => {
       });
   }
 };
+
+function displayVarukorg(){
+  recipeName.innerHTML = "";
+
+  for (i=0; i<varukorg.length; i++)
+  {
+    var myMeal = varukorg[i];
+
+    let count = 1;
+    let ingredients = [];
+        for (let i in myMeal) {
+            let ingredient = "";
+            let measure = "";
+            if (i.startsWith("strIngredient") && myMeal[i]) {
+                ingredient = myMeal[i];
+                if (myMeal[`strMeasure` + count]) {
+                    measure = myMeal[`strMeasure` + count];
+            } 
+                else {
+                    measure = "";
+                }
+            count += 1;
+            ingredients.push(`${measure} ${ingredient}`);
+          }
+    }
+    console.log("ingredients")
+
+    recipeName.innerHTML += `
+      <h2>${myMeal.strMeal}</h2>
+      <h3>Ingredients:</h3>
+      <p>${ingredients}</p>
+      <hr>
+      `;
+  }
+
+}
+
 window.addEventListener("load", getInfo);
 searchBtn.addEventListener("click", getInfo);
-
-// Get the modal
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var modalcontainer = document.getElementsByClassName("modal-content");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
